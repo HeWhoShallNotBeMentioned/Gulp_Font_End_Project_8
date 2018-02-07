bcc8c2e597f464855f9aa9a1cbf4ba116815f268
Chris Underwood <cunderwoodmn@gmail.com>
2018-02-06 23:59:27 -0800

    add scripts task to concat and create js map

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,21 @@
 'use strict';
 
-var gulp  = require('gulp');
+const gulp  = require('gulp');
+const concat  = require('gulp-concat');
+const maps    = require('gulp-sourcemaps');
 
 // gulp.task();
+
+gulp.task('scripts', function () {
+  gulp.src([
+    "js/circle/autogrow.js",
+    "js/circle/circle.js",
+    "js/global.js"])
+    .pipe(maps.init())
+   .pipe(concat("app.js"))
+   .pipe(maps.write('./'))
+  .pipe(gulp.dest("dist/scripts"));
+});
 
 gulp.task('default', function() {
 
